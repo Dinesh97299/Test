372a557f5a74c17c692bdcf99b8594cca489be47
Dinesh Kumar <dinesh.15.kumar.ext@nokia.com>
2025-07-29 17:43:34 +0530

    new file

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet_01"
+  location            = "west europe"
+  resource_group_name = "rg_09"
+  address_space       = ["10.0.0.0/16"]
+}
